fab8ca74d671a1a12fb4303702b61020c5f6e184
TH <tunxero@hotmail.com>
2025-03-26 12:09:30 +0200

    Added hard difficulty with randomized input, changed the starting position as well

--- main/main.ino
+++ main/main.ino
@@ -9,6 +9,7 @@ const int BTN_LEFT = 5;
 
 const int BTN_START = 6;
 const int BTN_RESET = 7;
+const int BTN_DIFFICULTY = 8;
 
 /* I2C
 * SDA Serial Data
@@ -60,11 +61,13 @@ struct GameObject {
  GRID_SLOT_SIDE defines only the part that the snake part can be within.
  So the game area has 1 pixel thick border all around which reduces the game area by 1 pixel on each side so 2 pixels horizontal and vertical
 */
-// NOTE(temppu) Check also the HEAD_COORD_XY and FRUIT TABLE
+// NOTE(temppu) Check also the INIT_POS_MULT and FRUIT TABLE
 const uint8_t GRID_SLOT_SIDE = 5;
 
 const uint8_t MAX_SNAKE_LEN = 100;
 
+// Multiplier to define the starting position NOTE(temppu) might collide with FRUIT_TABLE / ROCK_TABLE
+const uint8_t INIT_POS_MULT = 6;
 const uint8_t SNAKE_HEAD_RADIUS = 2;
 const uint8_t SNAKE_BODY_RADIUS = 1;
 
@@ -74,26 +77,21 @@ const uint8_t ROCK_RADIUS = 4;
 
 const uint8_t FRAME_THICKNESS = 1;
 
-// NOTE(temppu) If the GRID_SLOT_SIDE is odd, add 1 to it for this calculation
-// With GRID_SLOT_SIZE = 5, FRAME_THICKNESS = 1 the result should be 4, so head start @(4,4)
-// #WARNING CHANGE START POINT
-const uint8_t HEAD_COORD_XY = (GRID_SLOT_SIDE + 1) / 2;
-
-
-// NOTE(temppu) Coordinates in this table should have the same divisibility as the HEAD_COORD_XY
+// NOTE(temppu) Coordinates in this table should have the same divisibility as the INIT_POS_MULT
 const uint8_t FRUIT_TABLE[] = {
-  10, 50, 25, 90, 5,
-  40, 75, 10, 80, 15,
+  10, 50, 25, 90, 10,
+  40, 75, 10, 80, 20,
   70, 60, 65, 35, 25,
-  60, 30, 15, 50, 70
+  60, 30, 20, 50, 70
 };
 
 const uint8_t ROCK_TABLE[] = {
-  15, 45, 20, 85, 5,
-  55, 65, 15, 70, 10,
-  40, 60, 65, 35, 25,
-  65, 35, 15, 50, 70
+  45,  5, 85, 
+  55, 35, 45,
+  45, 65, 35, 
+  85, 15,  5
 };
+
 /*
 * So we have 128 pixels in each direction on the display. Width is reduced from 128 px to 126px for the game grid and with 5x5 px tile, we should use 125 as the limit spot.
 * pixels start from 0 so max pixel position is 127. With the previous specification, this is reduced for grid range [1, 126] which should give 126 pixels to work with.
@@ -102,8 +100,15 @@ const uint8_t ROCK_TABLE[] = {
 * and for height [0, 96] which gives 97 pixels of which 2px for borders and rest for game grid.
 * So the game grid has the size of 125 x 95 px
 */
-const struct DrawArea GAME_AREA = { .x_offset = 0, .y_offset = 0, .w = 127, .h = 97 };
+const struct DrawArea GAME_AREA = { .x_offset = 1, .y_offset = 1, .w = 125, .h = 95 };
 const struct DrawArea SCORE_AREA = { .x_offset = 0, .y_offset = 102, .w = 126, .h = 26 };
+
+const struct SnakeDirection KEYMAP[] = {
+  { 0, -1}, // UP
+  { 0,  1}, // DOWN
+  {-1,  0}, // LEFT
+  { 1,  0}  // RIGHT
+};
 
 volatile struct SnakeDirection dir = { 0, 0 };
 
@@ -116,12 +121,18 @@ volatile bool is_game_over = 0;
 // NOTE(temppu) This variable has double meaning, it is used as a basis for snake current length
 volatile int8_t score = 0;
 
+// NOTE a bit of a challenge
+volatile uint8_t input_button_offset = 0;
+volatile bool is_difficulty_hard = 0;
+
 // Function declarations:
 void read_input();
+void set_dir(uint8_t btn_idx);
 void move_snake();
 void init_game();
-void add_fruit();
+void add_fruit_rock();
 void add_snake_part();
+void set_difficulty();
 void game_over_screen();
 void draw_screen();
 
@@ -136,6 +147,7 @@ void setup() {
 
   pinMode(BTN_RESET, INPUT_PULLUP);
   pinMode(BTN_START, INPUT_PULLUP);
+  pinMode(BTN_DIFFICULTY, INPUT_PULLUP);
 
   // put your setup code here, to run once:
   u8g2.begin();
@@ -146,10 +158,10 @@ void setup() {
 
 void loop() {
   // Unfortunately, overhead from u8g2lib and not so efficient snake logic, the game is not that hard
-  /*int8_t d = 50 - score * 5;
-  if (d >= 5) {
-    delay(d);
-  }*/
+  int8_t reduce_delay = 50 - score * 5;
+  if (reduce_delay >= 5) {
+    delay(reduce_delay);
+  }
 
   if (is_game_over) {
     game_over_screen();
@@ -169,6 +181,7 @@ void loop() {
 * # NOTE(temppu) 
 * Do we want INTERRUPTS and thus propably bugs / undefined behaviour?
 */
+/* ORIGINAL
 void read_input() {
   if (!digitalRead(BTN_UP) && dir.y == 0) {
     dir.x = 0;
@@ -188,13 +201,58 @@ void read_input() {
     init_game();
   }
 }
+*/
+void read_input() {
+  if (!digitalRead(BTN_DIFFICULTY)) {
+    is_difficulty_hard = !is_difficulty_hard;
+    set_difficulty();
+  }
+  if (!digitalRead(BTN_UP)) {
+    set_dir(0);
+  } else if (!digitalRead(BTN_DOWN)) {
+    set_dir(1);
+  }
+  if (!digitalRead(BTN_LEFT)) {
+    set_dir(2);
+  } else if (!digitalRead(BTN_RIGHT)) {
+    set_dir(3);
+  }
+  if (!digitalRead(BTN_RESET)) {
+    init_game();
+  }
+}
+
+void set_dir(uint8_t btn_idx) {
+  // Get the offset to find from keymap, four inputs -> should be divisible by 4 
+  uint8_t key_idx = (btn_idx + input_button_offset) % 4;
+
+  // Check if trying to reverse where came from
+  if (KEYMAP[key_idx].x == -dir.x && KEYMAP[key_idx].y == -dir.y) {
+    return;
+  }
+
+  // Set the new direction
+  dir.x = KEYMAP[key_idx].x;
+  dir.y = KEYMAP[key_idx].y;
+}
+
+void set_difficulty() {
+  // Randomizes the inputs
+  if (is_difficulty_hard) {
+    input_button_offset = random(0,3);
+  } else {
+    input_button_offset = 0;
+  }
+}
 
 void init_game() {
   
   // Game set
   score = 0;
   is_game_over = 0;
-  add_fruit();
+  add_fruit_rock();
+
+  set_difficulty();
 
   // Start from stopped mode:
   dir.x = 0;
@@ -204,11 +262,11 @@ void init_game() {
   // This seeds the actual positioning of each snake part on the grid
   // The .x .y should be the CENTER of the grid when using CIRCLE as the bodypart
   // So, if we have 5x5 grid and the first slot starts from (1,1) and ends (6,6)
-  // which would result to .x = 4 .y = 4 as the head start
-  snake[0].x_pos = HEAD_COORD_XY;
-  snake[0].y_pos = HEAD_COORD_XY;
+  // so with GRID 5 and INIT POS MULT 3 => (15, 15) as start point 
+  snake[0].x_pos = GRID_SLOT_SIDE * INIT_POS_MULT;
+  snake[0].y_pos = GRID_SLOT_SIDE * INIT_POS_MULT;
   snake[0].radius = SNAKE_HEAD_RADIUS;
-    
+  
   // Rest of the body:
   for (uint8_t i = 1; i < MAX_SNAKE_LEN; i++) {
     snake[i].x_pos = 0;
@@ -224,20 +282,25 @@ void add_snake_part() {
   snake[score + 1].radius = SNAKE_BODY_RADIUS;
 }
 
-// Selects fruits spot from specified table "randomly".   
-void add_fruit() {
-  uint8_t size = (sizeof(FRUIT_TABLE) / sizeof(uint8_t));
-  int8_t r = random(0, size);
-  uint8_t x_idx = score % size;
-  uint8_t y_idx = r % size;
+// Selects fruits spot from specified table "randomly" the same is done to the rock.   
+void add_fruit_rock() {
+  uint8_t fruitT_size = (sizeof(FRUIT_TABLE) / sizeof(uint8_t));
+  uint8_t rockT_size = (sizeof(ROCK_TABLE) / sizeof(uint8_t));
+  
+  int8_t rf = random(0, fruitT_size);
+  int8_t rr = random(0, rockT_size);
+  
+  uint8_t x_idx = score % fruitT_size;
+  uint8_t y_idx = rf % fruitT_size;
+  uint8_t y_idx_rock = rr % rockT_size;
 
-  // Using the constant table FRUIT_TABLE we can easily get desired output that is easier to test:
-  fruit.x_pos = HEAD_COORD_XY + FRUIT_TABLE[x_idx];
-  fruit.y_pos = HEAD_COORD_XY + FRUIT_TABLE[y_idx];
+  // Using the constant table, we can easily get desired output that is easier to test:
+  fruit.x_pos = FRUIT_TABLE[x_idx];
+  fruit.y_pos = FRUIT_TABLE[y_idx];
   fruit.radius = FRUIT_RADIUS;
   
-  rock.x_pos = HEAD_COORD_XY + ROCK_TABLE[y_idx];
-  rock.y_pos = HEAD_COORD_XY + ROCK_TABLE[r % size];
+  rock.x_pos = ROCK_TABLE[y_idx_rock];
+  rock.y_pos = ROCK_TABLE[rr % rockT_size];
   rock.radius = ROCK_RADIUS;
 }
 
@@ -254,7 +317,7 @@ void move_snake() {
   int8_t n_y = snake[0].y_pos + dir.y * GRID_SLOT_SIDE;
 
   // Checks whether the snake goes out of GAME_AREA bounds:
-  if (GAME_AREA.x_offset > n_x || GAME_AREA.w < n_x || GAME_AREA.y_offset > n_y || GAME_AREA.h < n_y) {
+  if (GAME_AREA.x_offset >= n_x || GAME_AREA.w <= n_x || GAME_AREA.y_offset >= n_y || GAME_AREA.h <= n_y) {
     is_game_over = 1;
     return;
   }
@@ -271,7 +334,7 @@ void move_snake() {
     // Score is used when iterating through the snake
     score++;
     add_snake_part();
-    add_fruit();
+    add_fruit_rock();
   }
 
   // We need to iterate through the snake to check if head collides with the body.
@@ -328,9 +391,15 @@ void draw_screen(void) {
     // Here are some "magic" numbers that affect the position of the text at this time
     u8g2.setFont(u8g2_font_t0_11_mf);
     u8g2.drawStr(SCORE_AREA.x_offset + 8, SCORE_AREA.y_offset + 14, "Score:");
-    u8g2.setCursor(SCORE_AREA.x_offset + 10 + 8 * 5 + 4, SCORE_AREA.y_offset + 14);
+    u8g2.setCursor(SCORE_AREA.x_offset + 8 + 8 * 5 + 4, SCORE_AREA.y_offset + 14);
     u8g2.print(score);
-
+   
+    if (is_difficulty_hard) {
+      u8g2.drawStr(SCORE_AREA.x_offset + 8 + 8 * 5 + 12, SCORE_AREA.y_offset + 14, "HARD");
+    } else {
+      u8g2.drawStr(SCORE_AREA.x_offset + 8 + 8 * 5 + 12, SCORE_AREA.y_offset + 14, "EASY");
+    }
+    
     // Draw game frame:
     u8g2.drawFrame(GAME_AREA.x_offset, GAME_AREA.y_offset, GAME_AREA.w, GAME_AREA.h);
 
@@ -347,3 +416,4 @@ void draw_screen(void) {
 
   } while (u8g2.nextPage() && !is_game_over);
 }
+
